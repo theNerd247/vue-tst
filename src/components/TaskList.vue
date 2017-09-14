86f1d8a0a0eee5eb3d420a9a2497cac23456b5d2
theNerd247 <noah.harvey247 gm>
2017-09-14 19:56:41 -0400

    adds vuelidate

--- src/components/TaskList.vue
+++ src/components/TaskList.vue
@@ -1,6 +1,27 @@
+
 <template>
 	<div id="tasklist">
 		<h1> {{ name }} </h1>
+
+		<h2>New Task</h2>
+		<b-form
+			@submit="onSubmit"
+			inline
+			:class="{'was-validated': !$v.form.$error }"
+			>
+			<b-form-group id='fGroup1'>
+				<b-form-input
+					id="taskDesc1"
+					type="text"
+					placeholder="description"
+					v-model="form.taskDesc"
+					@input="$v.form.taskDesc.$touch()"
+					>
+				</b-form-input>
+				<b-button v-if="!$v.form.$error" type="submit" variant="primary">Submit</b-button>
+			</b-form-group>
+			<b-form-feedback></b-form-feedback>
+		</b-form>
 
 		<!-- Not Done Tasks-->
 		<h2>On Going</h2>
@@ -9,6 +30,7 @@
 				<span id='taskDesc'> {{ task.desc }} </span>
 				<span id='taskDateCreate'> {{ task.dateCreate }} </span>
 				<b-button :variant="'danger'" @click='toggleDoneness(task)'>Done</b-button>
+				<b-button :variant="'warning'" @click='deleteItem(index)'>Delete</b-button>
 			</b-list-group-item>
 		</b-list-group>
 
@@ -19,53 +41,89 @@
 				<span id='taskDesc'> {{ task.desc }} </span>
 				<span id='taskDateCreate'> {{ task.dateCreate }} </span>
 				<b-button :variant="'success'" @click='toggleDoneness(task)'>Un-Done-me</b-button>
+				<b-button :variant="'warning'" @click='deleteItem(index)'>Delete</b-button>
 			</b-list-group-item>
 		</b-list-group>
+
 	</div>
 </template>
 
 <script>
-	export default {
-		name: 'hello',
-		data: function () {
-			return {
-				name: "TaskList",
-				tasks: [
-					{ desc: "This is a task",
-						dateCreate: new Date('2017-08-01'),
-						dateDone: null
-					},
-					{ desc: "This is a task 2",
-						dateCreate: new Date('2017-08-02'),
-						dateDone: null
-					},
-					{ desc: "This is a task",
-						dateCreate: new Date('2017-08-01'),
-						dateDone: new Date('2017-09-15')
-					},
-					{ desc: "This is a task 2",
-						dateCreate: new Date('2017-08-02'),
-						dateDone: new Date('2017-09-15')
-					},
-					{ desc: "This is a task",
-						dateCreate: new Date('2017-08-01'),
-						dateDone: new Date('2017-09-15')
-					},
-					{ desc: "This is a task 2",
-						dateCreate: new Date('2017-08-02'),
-						dateDone: new Date('2017-09-15')
-					}				]
-			}
+	import { alphaNum, required, minLength, between } from 'vuelidate/lib/validators'
+export default {
+	name: 'hello',
+	data: function () {
+		return {
+			form: {
+				taskDesc: ""
+			},
+			name: "TaskList",
+			tasks: [
+				{ desc: "This is a task",
+					dateCreate: new Date('2017-08-01'),
+					dateDone: null
+				},
+				{ desc: "This is a task 2",
+					dateCreate: new Date('2017-08-02'),
+					dateDone: null
+				},
+				{ desc: "This is a task",
+					dateCreate: new Date('2017-08-01'),
+					dateDone: new Date('2017-09-15')
+				},
+				{ desc: "This is a task 2",
+					dateCreate: new Date('2017-08-02'),
+					dateDone: new Date('2017-09-15')
+				},
+				{ desc: "This is a task",
+					dateCreate: new Date('2017-08-01'),
+					dateDone: new Date('2017-09-15')
+				},
+				{ desc: "This is a task 2",
+					dateCreate: new Date('2017-08-02'),
+					dateDone: new Date('2017-09-15')
+				}
+			]
+		}
+	},
+	mounted: function () {
+		this.$v.form.$touch();
+	},
+	methods: {
+		removeTasks: function () {
+			this.tasks = null;
 		},
-		methods: {
-			toggleDoneness: function (task) {
-				if(task.dateDone == null)
-					task.dateDone = new Date();
-				else
-					task.dateDone = null;
+		deleteItem: function (index) {
+			this.tasks.splice(index,1);
+		},
+		toggleDoneness: function (task) {
+			if(task.dateDone == null)
+				task.dateDone = new Date();
+			else
+				task.dateDone = null;
+		},
+		onSubmit: function (evnt) {
+			evnt.preventDefault();
+			//process form submit
+			this.tasks.push({
+				desc: this.form.taskDesc,
+				dateCreate: new Date(),
+				dateDone: null
+			});
+		}
+	},
+	validations: {
+		tasks: {
+			required
+		},
+		form: {
+			taskDesc: {
+				required,
+				minLength: minLength(1)
 			}
 		}
 	}
+}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
--- src/main.js
+++ src/main.js
@@ -4,12 +4,14 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import BootstrapVue from 'bootstrap-vue'
+import Vuelidate from 'vuelidate'
 
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import 'bootstrap/dist/css/bootstrap.css'
 
 Vue.config.productionTip = false
 Vue.use(BootstrapVue);
+Vue.use(Vuelidate);
 
 /* eslint-disable no-new */
 new Vue({
